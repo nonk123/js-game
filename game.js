84346c53fdcd50f8420f996ad989aaa35d96cdd5
nonk123 <Sergeyii321@gmail.com>
2020-03-14 17:31:03 +0300

    Add basic map generation

--- game.js
+++ game.js
@@ -1,10 +1,43 @@
 let div = document.getElementById("game");
 
-var counter = 1;
+let mapWidth = 35;
+let mapHeight = 20;
+
+let map = Array(mapHeight);
+
+for (y = 0; y < mapHeight; y++) {
+    map[y] = Array(mapWidth);
+}
+
+function insertTileAt(tile, x, y) {
+    map[Math.floor(y)][Math.floor(x)] = tile;
+}
+
+function fillMap(tile) {
+    for (row of map) {
+        row.fill(tile);
+    }
+}
 
 function tick() {
-    div.innerHTML = "<tr><th>Ticked " + counter++ + "</th></tr>";
+    var table = "";
+
+    for (row of map) {
+        table += "<tr>";
+
+        for (col of row) {
+            style = "font-family:monospace";
+            table += "<td style=\"" + style + "\">" + col + "</td>";
+        }
+
+        table += "</tr>";
+    }
+
+    div.innerHTML = table;
 }
+
+fillMap(".");
+insertTileAt("@", mapWidth / 2, mapHeight / 2)
 
 tick();
 setInterval(tick, 500);
