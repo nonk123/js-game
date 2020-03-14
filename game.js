9ab337d63094d243675f78a353981529abab8e9f
nonk123 <Sergeyii321@gmail.com>
2020-03-14 17:56:24 +0300

    Better tile handling

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-let div = document.getElementById("game");
+let mapElement = document.getElementById("game");
 
 let mapWidth = 35;
 let mapHeight = 20;
@@ -9,13 +9,54 @@ for (y = 0; y < mapHeight; y++) {
     map[y] = Array(mapWidth);
 }
 
+let defaultFg = "white"
+let defaultBg = "black"
+
+class Tile {
+    constructor(character, fg=defaultFg, bg=defaultBg) {
+        this._character = character;
+        this._fg = fg;
+        this._bg = bg;
+    }
+
+    get character() {
+        return this._character;
+    }
+
+    set character(character) {
+        this._character = character;
+    }
+
+    get fg() {
+        return this._fg;
+    }
+
+    set fg(fg) {
+        this._fg = fg;
+    }
+
+    get bg() {
+        return this._bg;
+    }
+
+    set bg(bg) {
+        this._bg = bg;
+    }
+}
+
 function insertTileAt(tile, x, y) {
-    map[Math.floor(y)][Math.floor(x)] = tile;
+    if (tile instanceof Tile) {
+        map[Math.floor(y)][Math.floor(x)] = tile;
+    } else {
+        console.log("Not a tile");
+    }
 }
 
 function fillMap(tile) {
-    for (row of map) {
-        row.fill(tile);
+    for (y = 0; y < mapHeight; y++) {
+        for (x = 0; x < mapWidth; x++) {
+            insertTileAt(tile, x, y);
+        }
     }
 }
 
@@ -25,19 +66,20 @@ function tick() {
     for (row of map) {
         table += "<tr>";
 
-        for (col of row) {
-            style = "font-family:monospace";
-            table += "<td style=\"" + style + "\">" + col + "</td>";
+        for (tile of row) {
+            style = "font-family:monospace;color:" + tile.fg + ";background:"
+                + tile.bg + ";border-spacing=0px";
+            table += "<td style=\"" + style + "\">" + tile.character + "</td>";
         }
 
         table += "</tr>";
     }
 
-    div.innerHTML = table;
+    mapElement.innerHTML = table;
 }
 
-fillMap(".");
-insertTileAt("@", mapWidth / 2, mapHeight / 2)
+fillMap(new Tile("."));
+insertTileAt(new Tile("@"), mapWidth / 2, mapHeight / 2);
 
 tick();
 setInterval(tick, 500);
